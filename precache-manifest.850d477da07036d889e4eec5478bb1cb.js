a0ebe245976641bd87e35314865e90d499a6805c
Kiartichai0 <kiartichai.su.61@ubu.ac.th>
2020-09-02 10:58:17 +0700

    Updates

---
+++ precache-manifest.850d477da07036d889e4eec5478bb1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb735563a40bb3eb159ef5a83db78a6a",
+    "url": "/Kiartichai0/react_2-9-2563/index.html"
+  },
+  {
+    "revision": "d1132d74b23006b9ff8e",
+    "url": "/Kiartichai0/react_2-9-2563/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "100bd38748e9ee5eb4a8",
+    "url": "/Kiartichai0/react_2-9-2563/static/js/2.4bd49244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kiartichai0/react_2-9-2563/static/js/2.4bd49244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1132d74b23006b9ff8e",
+    "url": "/Kiartichai0/react_2-9-2563/static/js/main.5770503b.chunk.js"
+  },
+  {
+    "revision": "f828844c1d45d94d0424",
+    "url": "/Kiartichai0/react_2-9-2563/static/js/runtime-main.04cfc06a.js"
+  }
+]);
